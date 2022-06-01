dd9ec907bcee99ddeaa846fd317ad3be75a4ece2
Hendi Permana <hendi@eluon.com>
2022-06-02 00:40:18 +0700

    export Db

--- database/annedb.sql
+++ database/annedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Bulan Mei 2022 pada 14.15
+-- Waktu pembuatan: 01 Jun 2022 pada 19.39
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 7.4.23
 
@@ -24,30 +24,74 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kegiatan`
+-- Struktur dari tabel `anggaran`
 --
 
-CREATE TABLE `kegiatan` (
-  `id` int(4) NOT NULL,
-  `nama_kegiatan` varchar(45) NOT NULL,
-  `organisasi` varchar(45) NOT NULL,
+CREATE TABLE `anggaran` (
+  `id_anggaran` int(10) NOT NULL,
   `tanggal` date NOT NULL,
+  `nominal` int(20) NOT NULL,
+  `no_rekening` varchar(25) NOT NULL,
   `keterangan` text NOT NULL,
-  `status` int(2) NOT NULL
+  `type_anggaran` int(2) NOT NULL,
+  `id_kegiatan` int(4) NOT NULL,
+  `status` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pegawai`
+-- Struktur dari tabel `donatur`
 --
 
-CREATE TABLE `pegawai` (
-  `id` int(11) NOT NULL,
-  `nama_pegawai` varchar(45) NOT NULL,
-  `no_pegawai` varchar(45) NOT NULL,
+CREATE TABLE `donatur` (
+  `id_donatur` int(11) NOT NULL,
+  `nama_donatur` varchar(45) NOT NULL,
+  `kontak` varchar(45) NOT NULL,
+  `no_rekening` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `id_kegiatan` int(4) NOT NULL,
+  `nama_kegiatan` varchar(45) NOT NULL,
+  `lokasi` varchar(45) NOT NULL,
+  `tanggal` date NOT NULL,
+  `keterangan` text NOT NULL,
+  `status` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kegiatan`
+--
+
+INSERT INTO `kegiatan` (`id_kegiatan`, `nama_kegiatan`, `lokasi`, `tanggal`, `keterangan`, `status`) VALUES
+(4, 'Musa testing kegiatan', '', '2022-06-01', 'testing 1', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengurus`
+--
+
+CREATE TABLE `pengurus` (
+  `id_pengurus` int(11) NOT NULL,
+  `nama_pengurus` varchar(45) NOT NULL,
+  `no_pengurus` varchar(45) NOT NULL,
   `jabatan` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pengurus`
+--
+
+INSERT INTO `pengurus` (`id_pengurus`, `nama_pengurus`, `no_pengurus`, `jabatan`) VALUES
+(5, 'anne kepala bagian', '0123456', 1);
 
 -- --------------------------------------------------------
 
@@ -59,53 +103,84 @@ CREATE TABLE `user` (
   `user_name` varchar(45) NOT NULL,
   `password` varchar(10) NOT NULL,
   `user_type` varchar(10) NOT NULL,
-  `id` int(5) NOT NULL,
-  `no_pegawai` varchar(45) NOT NULL
+  `id_user` int(5) NOT NULL,
+  `no_pengurus` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`user_name`, `password`, `user_type`, `id_user`, `no_pengurus`) VALUES
+('kepala1', 'kepala', '1', 9, '0123456');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indeks untuk tabel `anggaran`
+--
+ALTER TABLE `anggaran`
+  ADD PRIMARY KEY (`id_anggaran`);
+
+--
+-- Indeks untuk tabel `donatur`
+--
+ALTER TABLE `donatur`
+  ADD PRIMARY KEY (`id_donatur`);
+
+--
 -- Indeks untuk tabel `kegiatan`
 --
 ALTER TABLE `kegiatan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_kegiatan`);
 
 --
--- Indeks untuk tabel `pegawai`
+-- Indeks untuk tabel `pengurus`
 --
-ALTER TABLE `pegawai`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `pengurus`
+  ADD PRIMARY KEY (`id_pengurus`);
 
 --
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `anggaran`
+--
+ALTER TABLE `anggaran`
+  MODIFY `id_anggaran` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `donatur`
+--
+ALTER TABLE `donatur`
+  MODIFY `id_donatur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `kegiatan`
 --
 ALTER TABLE `kegiatan`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_kegiatan` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `pegawai`
+-- AUTO_INCREMENT untuk tabel `pengurus`
 --
-ALTER TABLE `pegawai`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `pengurus`
+  MODIFY `id_pengurus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
