0494b95b1303fd04244f721f5cb23e96427a54a6
Hendi Permana <hendi@eluon.com>
2022-06-03 18:02:44 +0700

    export db

--- database/dinkop.sql
+++ database/dinkop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Jun 2022 pada 19.39
+-- Waktu pembuatan: 03 Jun 2022 pada 13.01
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 7.4.23
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `annedb`
+-- Database: `dinkop`
 --
 
 -- --------------------------------------------------------
@@ -38,18 +38,17 @@ CREATE TABLE `anggaran` (
   `status` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Struktur dari tabel `donatur`
+-- Dumping data untuk tabel `anggaran`
 --
 
-CREATE TABLE `donatur` (
-  `id_donatur` int(11) NOT NULL,
-  `nama_donatur` varchar(45) NOT NULL,
-  `kontak` varchar(45) NOT NULL,
-  `no_rekening` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `anggaran` (`id_anggaran`, `tanggal`, `nominal`, `no_rekening`, `keterangan`, `type_anggaran`, `id_kegiatan`, `status`) VALUES
+(3, '2022-05-19', 20, '20', 'test3', 1, 0, 2),
+(5, '2022-05-17', 700000, '', 'Konsumsi', 0, 2, 2),
+(6, '2022-05-17', 1000, '', 'test Edit Pengeluaran2', 0, 2, 2),
+(11, '2022-06-01', 2510001, '12468789451', 'dana desa', 1, 0, 0),
+(12, '2022-05-17', 100, '', 'testing', 0, 2, 0),
+(13, '2022-05-17', 10, '', 'testing', 0, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -60,7 +59,7 @@ CREATE TABLE `donatur` (
 CREATE TABLE `kegiatan` (
   `id_kegiatan` int(4) NOT NULL,
   `nama_kegiatan` varchar(45) NOT NULL,
-  `lokasi` varchar(45) NOT NULL,
+  `organisasi` varchar(45) NOT NULL,
   `tanggal` date NOT NULL,
   `keterangan` text NOT NULL,
   `status` int(2) NOT NULL
@@ -70,28 +69,32 @@ CREATE TABLE `kegiatan` (
 -- Dumping data untuk tabel `kegiatan`
 --
 
-INSERT INTO `kegiatan` (`id_kegiatan`, `nama_kegiatan`, `lokasi`, `tanggal`, `keterangan`, `status`) VALUES
-(4, 'Musa testing kegiatan', '', '2022-06-01', 'testing 1', 0);
+INSERT INTO `kegiatan` (`id_kegiatan`, `nama_kegiatan`, `organisasi`, `tanggal`, `keterangan`, `status`) VALUES
+(2, 'Training PNS', 'dinkop', '2022-05-17', 'TESTING', 0),
+(3, 'Musa testing kegiatan', 'dinkop1', '2022-06-01', 'testing', 0);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengurus`
+-- Struktur dari tabel `pegawai`
 --
 
-CREATE TABLE `pengurus` (
-  `id_pengurus` int(11) NOT NULL,
-  `nama_pengurus` varchar(45) NOT NULL,
-  `no_pengurus` varchar(45) NOT NULL,
+CREATE TABLE `pegawai` (
+  `id_pegawai` int(11) NOT NULL,
+  `nama_pegawai` varchar(45) NOT NULL,
+  `nip` varchar(45) NOT NULL,
   `jabatan` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `pengurus`
+-- Dumping data untuk tabel `pegawai`
 --
 
-INSERT INTO `pengurus` (`id_pengurus`, `nama_pengurus`, `no_pengurus`, `jabatan`) VALUES
-(5, 'anne kepala bagian', '0123456', 1);
+INSERT INTO `pegawai` (`id_pegawai`, `nama_pegawai`, `nip`, `jabatan`) VALUES
+(1, 'Winda', '1510631170065009', 1),
+(2, 'Winda Staff', '7845123654', 3),
+(3, 'Winda Bendahara', '151063117006', 2),
+(5, 'Winda Pengguan Anggaran', '1245748516', 4);
 
 -- --------------------------------------------------------
 
@@ -104,15 +107,20 @@ CREATE TABLE `user` (
   `password` varchar(10) NOT NULL,
   `user_type` varchar(10) NOT NULL,
   `id_user` int(5) NOT NULL,
-  `no_pengurus` varchar(45) NOT NULL
+  `nip` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`user_name`, `password`, `user_type`, `id_user`, `no_pengurus`) VALUES
-('kepala1', 'kepala', '1', 9, '0123456');
+INSERT INTO `user` (`user_name`, `password`, `user_type`, `id_user`, `nip`) VALUES
+('winda', 'winda', '1', 3, '1510631170065'),
+('winda bendahara', 'bendahara', '2', 4, '1510631170067'),
+('winda admin', 'admin', '3', 5, '7845123654'),
+('master', 'master', '0', 6, ''),
+('gagal', 'gagal', '4', 7, ''),
+('winda Pengguna Anggaran1', 'pengguna', '4', 8, '1245748516');
 
 --
 -- Indexes for dumped tables
@@ -125,22 +133,16 @@ ALTER TABLE `anggaran`
   ADD PRIMARY KEY (`id_anggaran`);
 
 --
--- Indeks untuk tabel `donatur`
---
-ALTER TABLE `donatur`
-  ADD PRIMARY KEY (`id_donatur`);
-
---
 -- Indeks untuk tabel `kegiatan`
 --
 ALTER TABLE `kegiatan`
   ADD PRIMARY KEY (`id_kegiatan`);
 
 --
--- Indeks untuk tabel `pengurus`
+-- Indeks untuk tabel `pegawai`
 --
-ALTER TABLE `pengurus`
-  ADD PRIMARY KEY (`id_pengurus`);
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`id_pegawai`);
 
 --
 -- Indeks untuk tabel `user`
@@ -156,31 +158,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `anggaran`
 --
 ALTER TABLE `anggaran`
-  MODIFY `id_anggaran` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT untuk tabel `donatur`
---
-ALTER TABLE `donatur`
-  MODIFY `id_donatur` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_anggaran` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `kegiatan`
 --
 ALTER TABLE `kegiatan`
-  MODIFY `id_kegiatan` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kegiatan` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `pengurus`
+-- AUTO_INCREMENT untuk tabel `pegawai`
 --
-ALTER TABLE `pengurus`
-  MODIFY `id_pengurus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `pegawai`
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
